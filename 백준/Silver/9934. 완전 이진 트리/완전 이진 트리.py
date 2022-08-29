e27a66cb299e29c27f9a4a27e4e632d10089ba19
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2022-08-29 20:33:13 +0900

    [Silver I] Title: 완전 이진 트리, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -0,0 +1,22 @@
+def binary(b, depth):
+    if len(b)==1:
+        tree[depth].extend(b)
+        return
+    length=len(b)
+    mid = length//2
+    tree[depth].append(b[mid])
+    binary(b[:mid], depth+1)
+    binary(b[mid+1:], depth+1)
+
+N=int(input())
+# 순회한 값으로 역으로 트리를 유추함 
+# 깊이가 K인 이진 트리는 총 2 * K - 1 개의 노드로 이루어져 있다!!
+buildings = list(map(int, input().split()))
+tree = [[] for _ in range(N)]
+# 빌딩 리스트와 인덱스 0 입력
+binary(buildings, 0)
+for i in range(N):
+    if i==0:
+        print(tree[i][0])
+    else:
+        print(*tree[i])
