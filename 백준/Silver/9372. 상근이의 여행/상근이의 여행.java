f39d9dc30edc72ce148b7ea1b6dcfdafddfb58c2
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2022-09-03 16:11:50 +0900

    [Silver IV] Title: 상근이의 여행, Time: 1128 ms, Memory: 241776 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int TC = sc.nextInt();
+		for (int tc = 0; tc < TC; tc++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			for (int m = 0; m < M; m++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+			}
+			System.out.println(N - 1);
+		} // TC
+	}// main
+}
