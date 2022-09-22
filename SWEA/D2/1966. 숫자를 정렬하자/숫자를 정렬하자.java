c476879f24ca8ddc86c0111c4b10583c2ea77bc7
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2022-09-23 00:27:12 +0900

    [D2] Title: 숫자를 정렬하자, Time: 153 ms, Memory: 23,168 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -2,33 +2,74 @@
 import java.util.Scanner;
 
 public class Solution {
+
+	static int[] sorted;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int TC = sc.nextInt();
+
 		for (int tc = 1; tc <= TC; tc++) {
 			int N = sc.nextInt();
 			int[] arr = new int[N];
-			for (int i = 0; i < N; i++) {
+			StringBuilder sb = new StringBuilder();
+			sorted = new int[N];
+
+			for (int i = 0; i < N; i++)
 				arr[i] = sc.nextInt();
-			} // arr 채우기 완료
 
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N - 1 - i; j++) {
-					if (arr[j + 1] < arr[j]) {
-						int temp = arr[j];
-						arr[j] = arr[j + 1];
-						arr[j + 1] = temp;
-					}
-				}
+			// arr, left, right 삽입 후 재귀
+			mergeSort(arr, 0, N - 1);
 
-			} // 정렬 완료
 			System.out.print("#" + tc + " ");
-			for (int i = 0; i < N; i++) {
-				System.out.printf("%d ", arr[i]);
+			for (int i = 0; i < arr.length; i++) {
+				sb.append(arr[i] + " ");
 			}
-			System.out.println();
-		} // TC
+			System.out.println(sb);
+		}
 
-		sc.close();
-	}// main
+	}
+
+	static void mergeSort(int[] arr, int left, int right) {
+		// 왼 인덱스가 오 인덱스보다 작을때
+		if (left < right) {
+			int mid = (left + right) / 2;
+			mergeSort(arr, left, mid); // 왼쪽 분할
+			mergeSort(arr, mid + 1, right);// 오른쪽 분할
+			merge(arr, left, mid, right);// 정렬하며 합치기
+		}
+	}
+
+	static void merge(int[] arr, int left, int mid, int right) {
+		int L = left; // 왼쪽 시작 인덱스
+		int R = mid + 1; // 오른쪽 시작 인덱스
+		int idx = left; // sorted에 넣을 인덱스
+
+		while (L <= mid && R <= right) { // 왼 인덱스가 중간보다 작거나 같고, 오 인덱스가 맨오른쪽보다 작거나 같을 때만 반복
+			// 왼쪽이 오른쪽보다 작으면 ㅇㅋ하고 인덱스 왼 전진
+			if (arr[L] <= arr[R]) {
+				sorted[idx] = arr[L++];
+			}
+			// 반대면 ㅇㅋ하고 인덱스 오 전진
+			else {
+				sorted[idx] = arr[R++];
+			}
+			idx++;
+		}
+
+		// 위 과정 후 왼쪽 혹은 오른쪽에 남아있는 경우 처리 
+		if (L <= mid) { // 왼쪽이 남
+			while (L <= mid) {
+				sorted[idx++] = arr[L++];
+			}
+		} else { // 오른쪽이 남
+			while (R <= right)
+				sorted[idx++] = arr[R++];
+		}
+		// arr에 정렬된 원소 채우기 
+		for (int i = left; i <= right; i++) {
+			arr[i] = sorted[i];
+		}
+
+	}
 }
