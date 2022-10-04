ffac4481eeebbf0cbb87203783bad83115bdf62e
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2022-10-04 23:35:28 +0900

    [level 2] Title: 주식가격, Time: 54.38 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -10,17 +10,22 @@ class Solution {
         }// stack 완성 
     
         int i=0;
+        // 큐에서 하나씩 꺼내며 다음 원소가 현재보다 작으면 break
         while (!q.isEmpty()){
             int now = q.poll();
+            // 매 반복때마다 몇 초 동안 떨어지지 않는지 봐야하므로 카운트 변수 0으로 초기화
+            // 맨 마지막 원소면 그대로 0 들어감
             int cnt=0;
-            // System.out.println(q.toString());
+            // 큐에 있는 원소 순회
             for (int tmp : q){
-                // System.out.print("now is "+now+", tmp is "+tmp+"\n");
+                // 안에 원소가 존재해서 들어왔으면 cnt ++
                 cnt++;
+                // 현재 원소보다 작으면 가격 떨어진 것이므로 for문 중단
                 if (tmp < now){
                     break;
                 }
             }
+            // 가격 얼마동안 떨어지는지 순회 후 해당 인덱스에 cnt 카운트한 것 저장 
             answer[i] = cnt;
             i++;
         }
