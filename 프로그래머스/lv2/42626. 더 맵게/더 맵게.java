bed2f7768720a3bc337c57e1b3831ec212aadde1
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2022-10-04 18:04:52 +0900

    [level 2] Title: 더 맵게, Time: 1293.86 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int answer = 0;
+        
+        for (int i=0; i<scoville.length;i++){
+            pq.add(scoville[i]);
+        }
+        
+        while (pq.size()!=1){
+            int first = pq.peek();
+            if (first>=K){
+                break;
+            }
+            
+            int a = pq.poll();
+            int b = 0;
+            int news = 0;
+            if (!pq.isEmpty()){
+                b = pq.poll();
+                news = a+b*2;
+                pq.add(news);
+                answer+=1;
+            }
+            
+            // System.out.println(pq.size());
+        }
+        
+        if (pq.size() == 1 && pq.peek() < K){
+            answer=-1;
+        }
+        return answer;
+    }
+}
