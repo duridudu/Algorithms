b5911cbf2b6c194e0f78c8d19bc1eae752841de1
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2022-12-17 01:07:04 +0900

    [Silver V] Title: DNA, Time: 100 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.py
+++ 백준/Silver/1969. DNA/DNA.py
@@ -1,36 +1,34 @@
-n, m = map(int, input().split())
+N,M=map(int, input().split(" "))
+dna=[]
+for _ in range(N):
+    dna.append(list((map(str, input()))))
 
-arr = []
+resDNA=''
+cnt=0
+for i in range(M):
+    t,a,c,g=0,0,0,0
+    for j in range(N):
+        if dna[j][i]=='T':
+            t+=1
+        elif dna[j][i]=='A':
+            a+=1
+        elif dna[j][i]=='C':
+            c+=1
+        elif dna[j][i]=='G':
+            g+=1
+        
+    if max(a,c,g,t)==a:
+        resDNA+='A'
+        cnt+= (c+g+t)
+    elif max(a,c,g,t)==c:
+        resDNA+='C'
+        cnt+= (a+g+t)
+    elif max(a,c,g,t)==g:
+        resDNA+='G'
+        cnt+= (a+c+t)
+    elif max(a,c,g,t)==t:
+        resDNA+='T'
+        cnt+= (a+g+c)
 
-# 문자열을 list형식으로 담아준다
-for i in range(n):
-    arr.append(list(map(str, input())))
-
-cnt, hap = 0, 0
-result = ''
-for i in range(m):
-    a, c, g, t = 0, 0, 0, 0
-    for j in range(n):
-        if arr[j][i] == 'T':
-            t += 1
-        elif arr[j][i] == 'A':
-            a += 1
-        elif arr[j][i] == 'G':
-            g += 1
-        elif arr[j][i] == 'C':
-            c += 1
-    if max(a,c,g,t) == a:
-        result += 'A'
-        hap += c + g +t
-    elif max(a,c,g,t) == c:
-        result += 'C'
-        hap += a + g +t
-    elif max(a,c,g,t) == g:
-        result += 'G'
-        hap += a + c +t
-    elif max(a,c,g,t) == t:
-        result += 'T'
-        hap += c + g + a
-    
-print(result)
-print(hap)
+print(resDNA)
+print(cnt)
