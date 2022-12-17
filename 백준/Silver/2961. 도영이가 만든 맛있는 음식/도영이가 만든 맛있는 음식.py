3c0558e94b5b7a67a32da23252a9156671fbbcd4
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2022-12-17 21:46:00 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -6,17 +6,18 @@ res=1000000000
 for i in range(N):
     tastes.append(list(map(int, input().split(" "))))
 for i in range(N):
+    # 만들 수 있는 모든 경우의 수 조합
     combi=list(combinations(nums, i+1))
-    # print(combi)
     for j in range(len(combi)):
+        # 신맛과 쓴맛 초기화
         sin=1
         ssn=0
-        # print(combi[j])
+        # 주어진 맛에 대해 모든 경우의 수를 해본다
         for k in range(len(combi[j])):
-            # print(combi[j][k])
             sin*=tastes[combi[j][k]][0]
             ssn+=tastes[combi[j][k]][1]
         temp=abs(sin-ssn)
+        # 저번 맛보다 차가 적으면 res 갱신 
         if temp<res:
             res=temp
 print(res)
