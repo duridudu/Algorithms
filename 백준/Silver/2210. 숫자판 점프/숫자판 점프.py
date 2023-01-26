796bc099a4f4cbf939f912004f6bb56d9d648745
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2023-01-26 21:15:52 +0900

    [Silver II] Title: 숫자판 점프, Time: 604 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -1,23 +1,39 @@
-def dfs(x, y, number):
-    if len(number) == 6: #6자리 숫자가 만들어졌다면
-        if number not in result: #result에 없다면
-            result.append(number)
+arr = []
+# nums = []
+visited = []
+for i in range(5):
+    arr.append(list(map(str, input().split())))
+    visited.append([False, False, False, False, False])
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+res = []
+
+def dfs(x, y, nums):
+    # print(x,y, arr[x][y])
+    # global nums
+    if len(nums) == 6:
+        if nums not in res:
+            res.append(nums)
+            # print(res)
         return
-        
-    dx = [1, -1, 0, 0] #상하좌우 확인 x
-    dy = [0, 0, 1, -1] #상하좌우 확인 y
-    for k in range(4):
-        ddx = x + dx[k]
-        ddy = y + dy[k]
-        
-        if 0 <= ddx < 5 and 0 <= ddy < 5: #범위 내에 있다면
-            dfs(ddx, ddy, number + matrix[ddx][ddy]) #6글자가 될 때 까지 재귀
 
-#입력
-matrix = [list(map(str, input().split())) for _ in range(5)]
+    for n in range(4):
+        cx = x + dx[n]
+        cy = y + dy[n]
+        if 0 <= cx < 5 and 0 <= cy < 5:
+            if not visited[cx][cy]:
+                dfs(cx, cy, nums + arr[cx][cy])
 
-result = []
+
 for i in range(5):
     for j in range(5):
-        dfs(i, j, matrix[i][j]) #0,0부터 4,4까지 모두 검사
-print(len(result))
+        if not visited[i][j]:
+            # cnt = 0
+            # res = []
+            dfs(i, j, arr[i][j])
+            # print("here", "".join(nums))
+
+# print(res)
+print(len(res))
