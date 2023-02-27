fd255bc7812ae9be031a36731a4d38f26a9288be
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2023-02-27 21:51:43 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/README.md
+++ 프로그래머스/lv3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.47 ms
+메모리: 10.3 MB, 시간: 0.48 ms
 
 ### 구분
 
--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,33 +1,19 @@
 def dfs(computers, visited, i):
     # global answer
     for j in range(len(computers[i])):
-        # print("dfs의 i는 {}, j는 {}".format(i,j))
-        # if i==3 and j==5:
-        #     print("여기야아아아")
-        #     print(computers[i][j])
-        #     print(visited[j-1])
         if i!=j and computers[i][j]==1 and visited[j-1]!=True:
-            # print("지굼 i는요~", i)
-            # print("지굼 j는욕~~~~", j)
             visited[i-1]=True
-            # answer+=1
             visited[j-1]=True
-
             dfs(computers, visited, j)
     
-        
 
 def solution(n, computers):
-    # global answer
     answer=0
     visited=[False]*(n)
-    # print(visited)
     for i in range(len(computers)):
         for j in range(len(computers[i])):
             if i!=j and computers[i][j]==1 and visited[j-1]!=True:
                 dfs(computers, visited, i)
                 answer+=1
-    # print(visited)
-    # print(answer)
     answer+=visited.count(False)
     return answer
