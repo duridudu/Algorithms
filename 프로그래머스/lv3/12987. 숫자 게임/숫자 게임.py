b72bc2c7265de927863f34ca7e2b82f3d7e50a4e
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2023-04-14 20:54:07 +0900

    [level 3] Title: 숫자 게임, Time: 52.82 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -7,25 +7,19 @@ def solution(A, B):
     
     A=deque(A)
     B=deque(B)
-    # print(A,B)
+    
     i=0
     for _ in range(len(A)):
-    # while (True):
-        # print(A[0], B[i], i)
         ## B팀이 이기면 answer에 추가 
         ## A, B팀 가장 작은 숫자 pop
         if A[0] < B[0]:
             answer+=1
+            ## 좋은 경기였다.. A,B 둘 다 나가잇-
             A.popleft()
             B.popleft()
-            # print("--------",A.popleft())
-            # print("--------",B.popleft())
-            # i+=1
         ## A팀이 B팀보다 더 크면 pop하고 다음 A 선수 데려옴
         else:
-            # i+=1
-            # print("요기야")
+            ## 이기지 못하는 B팀은 버려잇-
             B.popleft()
-            
         
     return answer
