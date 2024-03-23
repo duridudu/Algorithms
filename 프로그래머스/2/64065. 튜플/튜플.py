30872d82e11ff10b485dd44598c7359fe7ff0f14
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2024-03-24 00:41:34 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = []
+    s = s[2:len(s)-2].split("},{")
+    temp = []
+    for nums in s:
+        n = nums.split(",")
+        n = list(map(int, n))
+        temp.append([len(n),n])
+    temp.sort(key=lambda x:x[0])
+    #print("now",temp)
+    answer.append(temp[0][1][0])
+    for t in range(1,len(temp)):
+        for tt in temp[t][1]:
+            #print("---", tt)
+            if tt not in temp[t-1][1]:
+                answer.append(int(tt))
+
+
+    return answer
