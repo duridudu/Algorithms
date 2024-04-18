733327b486a772d238db78c1dd09c2ee8561557d
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2024-04-18 23:36:05 +0900

    [level 2] Title: 올바른 괄호, Time: 18.06 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,40 @@
+# def solution(s):
+#     answer = True
+#     lst=[]
+#     temp=list(s)
+#     lst.append(temp[0])
+    
+#     for i in range(1,len(temp)):
+#         ## 비어있지 않으면
+#         if len(lst)!=0:
+#             now=lst[-1]
+#             if now=="(":
+#                 if temp[i]==")":
+#                     lst.pop()
+#                     continue
+#                 else:
+#                     lst.append(temp[i])
+#             elif now==")":
+#                 lst.append(temp[i])
+#         ## 비어있으면 넣어준다
+#         else:
+#             lst.append(temp[i])
+#     if len(lst)!=0:
+#         answer=False
+    
+#     return answer
+
+## 인상깊은 풀이... 이면서 따라해볼 수 있는 수준의,,,,
+def solution(s):
+    tmp = []
+    for i in s:
+        if len(tmp) == 0:
+            tmp.append(i)  
+        else:
+            if tmp[-1] + i == '()':
+                tmp.pop()
+                pass
+            else:
+                tmp.append(i)
+
+    return len(tmp)==0
