e7428c5cab2144086c87d5dfa155474fb9e63d9f
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2024-08-18 17:46:39 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,30 +1,30 @@
 import sys
-n = int(sys.stdin.readline())
-lst = []
-for _ in range(n) : 
-    order = sys.stdin.readline().split()
-    
-    if order[0] == 'push':
-        lst.append(order[1])
-        
-    elif order[0] == 'pop':
-        if len(lst)==0:
-            print(-1)
-        else :
-            a = lst.pop()
-            print(a)
-            
-    elif order[0] == 'size':
-        print(len(lst))
-        
-    elif order[0] == 'empty':
-        if len(lst)==0:
-            print(1)
-        else :
-            print(0)
-            
-    elif order[0] == 'top':
-        if len(lst) == 0:
-            print(-1)
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n) :
+    lst = input().split()
+    a = lst[0]
+   
+    if a == "push" :
+        b = lst[1]
+        stack.append(b)
+    elif a == "pop":
+        if len(stack) != 0 : 
+            print(stack.pop())
         else:
-            print(lst[-1])
+            print(-1)
+    elif a == "size":
+        print(len(stack))
+    elif a == "empty":
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif a =="top":
+        if len(stack) != 0:
+            print(stack[-1])
+        else:
+            print(-1)
