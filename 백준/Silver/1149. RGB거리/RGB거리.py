cf436aea015b28b39c655d8c9fc15619ef3f0651
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2024-09-15 23:17:10 +0900

    [Silver I] Title: RGB거리, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,14 +1,17 @@
 n = int(input())
-cities = []
+lst = []
+dp = [[0]*3 for _ in range(n)]
 
-calcur = [[0]*3 for _ in range(n)]
-for i in range(n):
-    cities.append(list(map(int, input().split())))
+for _ in range(n):
+  lst.append(list(map(int, input().split())))
 
-for i in range(n):
-    #print(cities, calcur)
-    calcur[i][0] = min(cities[i][0] + calcur[i-1][1], cities[i][0] + calcur[i-1][2])
-    calcur[i][1] = min(cities[i][1] + calcur[i-1][0], cities[i][1] + calcur[i-1][2])
-    calcur[i][2] = min(cities[i][2] + calcur[i-1][0], cities[i][2] + calcur[i-1][1])
+dp[0][0] = lst[0][0]
+dp[0][1] = lst[0][1]
+dp[0][2] = lst[0][2]
 
-print(min(calcur[n-1]))
+for i in range(1,n):
+  dp[i][0] = lst[i][0] + min(dp[i-1][1], dp[i-1][2])
+  dp[i][1] = lst[i][1] + min(dp[i-1][0], dp[i-1][2])
+  dp[i][2] = lst[i][2] + min(dp[i-1][0], dp[i-1][1])
+  
+print(min(dp[-1]))
