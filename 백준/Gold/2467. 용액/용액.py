1dbf836119b85298706120237ab97eaa5ed69c4b
Yunju-Lee <84007616+duridudu@users.noreply.github.com>
2024-09-24 23:58:31 +0900

    [Gold V] Title: 용액, Time: 184 ms, Memory: 54720 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,39 +1,22 @@
-# n=int(input())
-# tops = list(map(int, input().split()))
-# answer = []
-# for i in range(len(tops)):
-#     now = tops.pop()
-#     idx = -1
-#     #print("**********", i, tops)
-#     while(True):
-#         #print("before",idx, now, next)
-#         if (idx*(-1) == len(tops)+1) or len(tops)==0:
-#             answer.append(0)
-#             break
-#         next = tops[idx]
-#         #print(idx, now, next)
-#         if next > now:
-#             answer.append(n+idx-i)
-#             break
-#         else :
-#             idx-=1
-#    # print(answer)
-# answer.reverse()
-# print(answer)
+import sys
+input = sys.stdin.readline
 
-n = int(input()) # n=5
 
-medicine = list(map(int,input().split()))
+n = int(input())
+liquids = list(map(int, input().split()))
+answer = []
 
-med = []
-for m in medicine :
-    med.append([abs(m), m])
-med.sort(key=lambda x:x[0])
-merge = []
-for i in range(n-1):
-    merge.append([i,abs(med[i][1]+med[i+1][1])])
-merge.sort(key=lambda x:x[1])
-idx = merge[0][0]
-answer = [med[idx][1], med[idx+1][1]]
-answer.sort()
-print(*answer)
+start = 0
+end = len(liquids)-1
+
+while(start<end):
+  res = liquids[start]+liquids[end]
+  answer.append([abs(res), (liquids[start], liquids[end])])
+  if res>=0 :
+    end-=1
+  else:
+    start+=1
+  
+answer.sort(key=lambda x: (x[0]))
+
+print(answer[0][1][0], answer[0][1][1])
